57b34600bc967a5a50dd59c5bd19ebe7649cb411
root <root@ip-172-31-19-152.ap-northeast-2.compute.internal>
2018-05-04 20:25:29 +0000

    편의기능추가

--- cbnu_lib/views.py
+++ cbnu_lib/views.py
@@ -7,7 +7,7 @@ from cbnu_lib import wooyeol
 def add_enter(get_list):
     count = 0
     for n in get_list:
-          if (count % 8) == 0:
+          if (count % 7) == 0:
              get_list.insert(count,"\n")
           count = count + 1
     return get_list
@@ -20,7 +20,7 @@ def keyboard(request):
  
     return JsonResponse({
         'type':'buttons',
-        'buttons':['중앙도서관','과기도']
+        'buttons':['중앙도서관','과기도','편의기능']
     })
  
 @csrf_exempt
@@ -49,7 +49,7 @@ def answer(request):
         string = get_string(add_enter(crawl))
         return JsonResponse({
                 'message': {
-                    'text': "--------------------\n\n과학 기술 도서관 열람실 현황 입니다.\n\nㅇ좌석:" + str(count) + " / 180 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
+                    'text': "--------------------\n\n과학 기술 도서관 열람실 현황 입니다.\n\nㅇ사용가능좌석: " + str(count) + " / 180 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
                     'photo': {
                         'url': "http://cfile26.uf.tistory.com/image/9955E7385AEAEF962E9891",
                         'width': 960,
@@ -58,7 +58,7 @@ def answer(request):
                 },
 	        'keyboard': {
                     'type':'buttons',
-                    'buttons':['중앙도서관','과기도']
+                    'buttons':['중앙도서관','과기도','편의기능']
                 } 
             })
     elif datacontent == '뒤로가기':
@@ -70,16 +70,55 @@ def answer(request):
                 },
                 'keyboard': {
                     'type':'buttons',
-                    'buttons':['중앙도서관','과기도']
+                    'buttons':['중앙도서관','과기도','편의기능']
                 } 
             })
+    elif datacontent == '편의기능':
+        #crawl = "test"
+        return JsonResponse({
+                'message': {
+                    'text': "기능을 선택해주세요."
+                },
+                'keyboard': {
+                    'type':'buttons',
+                    'buttons':['예약연장 및 반납','뒤로가기']
+                } 
+            })
+    elif datacontent == '예약연장 및 반납':
+        #crawl = "test"
+        return JsonResponse({
+                'message': {
+                    'text': "예약연장 및 반납 링크로 이동합니다.",
+                    'message_button': {
+                       'label': "이동하기",
+                       'url': "http://libroom.chungbuk.ac.kr/login.aspx"
+                    }
+                },
+                'keyboard': {
+                    'type':'buttons',
+                    #'buttons':['예약연장','날씨']
+                    'buttons':['중앙도서관','과기도','편의기능']
+                } 
+            })
+#아직구현안함
+    elif datacontent == '날씨':
+        return JsonResponse({
+                'message': {
+                    'text': "미구현입니다."
+                },
+                'keyboard': {
+                    'type':'buttons',
+                    'buttons':['중앙도서관','과기도','편의기능']
+                } 
+            })
+#고렇다고합니다
     elif datacontent == '1열람실':
         crawl = wooyeol.get_crawl("1")
         count = len(crawl)
         string = get_string(add_enter(crawl))
         return JsonResponse({
                 'message': {
-                    'text': "--------------------\n\n중앙도서관 1열람실 현황 입니다.\n\nㅇ좌석:" + str(count) + " / 360 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
+                    'text': "--------------------\n\n중앙도서관 1열람실 현황 입니다.\n\nㅇ사용가능좌석: " + str(count) + " / 360 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
                     'photo': {
                         'url': "http://cfile29.uf.tistory.com/image/99BE78385AEAEF9422D8E5",
                         'width': 960,
@@ -88,7 +127,7 @@ def answer(request):
                 },
 	        'keyboard': {
                     'type':'buttons',
-                    'buttons':['중앙도서관','과기도']
+                    'buttons':['중앙도서관','과기도','편의기능']
                 } 
             })
     elif datacontent == '2열람실':
@@ -97,7 +136,7 @@ def answer(request):
         string = get_string(add_enter(crawl))
         return JsonResponse({
                 'message': {
-                    'text': "--------------------\n\n중앙도서관 2열람실 현황 입니다.\n\nㅇ좌석:" + str(count) + " / 264 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
+                    'text': "--------------------\n\n중앙도서관 2열람실 현황 입니다.\n\nㅇ사용가능좌석: " + str(count) + " / 264 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
                     'photo': {
                         'url': "http://cfile7.uf.tistory.com/image/9982A6385AEAEF950FC8F2",
                         'width': 960,
@@ -106,7 +145,7 @@ def answer(request):
                 },
 	        'keyboard': {
                     'type':'buttons',
-                    'buttons':['중앙도서관','과기도']
+                    'buttons':['중앙도서관','과기도','편의기능']
                 } 
             })
     elif datacontent == '3열람실':
@@ -115,7 +154,7 @@ def answer(request):
         string = get_string(add_enter(crawl))
         return JsonResponse({
                 'message': {
-                    'text': "--------------------\n\n중앙도서관 3 열람실 현황 입니다.\n\nㅇ좌석:" + str(count) + " / 408 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
+                    'text': "--------------------\n\n중앙도서관 3 열람실 현황 입니다.\n\nㅇ사용가능좌석: " + str(count) + " / 408 \nㅇ남은좌석\n\n--------------------\n" + string + "\n\n--------------------",
                     'photo': {
                         'url': "http://cfile24.uf.tistory.com/image/99635C385AEAEF952D88A7",
                         'width': 960,
@@ -124,7 +163,7 @@ def answer(request):
                 },
 	        'keyboard': {
                     'type':'buttons',
-                    'buttons':['중앙도서관','과기도']
+                    'buttons':['중앙도서관','과기도','편의기능']
                 } 
             })
 
